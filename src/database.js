c24e98e88ccc0412256a88b7e33bb7af7cc1b2ea
Julián David Gamboa <juliandg34@gmail.com>
2019-03-27 21:21:30 -0500

    falta vista de usuario

--- src/database.js
+++ src/database.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://juliandg34:<j1a9n91996>@cluster0-owv7k.mongodb.net/test?retryWrites=true', {
     useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
+    useNewUrlParser: true
 }).then(db => console.log('Base de datos conectada'))
 .catch(err => console.error(err));

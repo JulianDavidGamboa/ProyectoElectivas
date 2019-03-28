8ca06f3bb7585671d4c98e57e67014529a58a68f
Julián David Gamboa <juliandg34@gmail.com>
2019-03-28 17:58:42 -0500

    falta vista de usuarios

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://juliandg34:<j1a9n91996>@cluster0-owv7k.mongodb.net/test?retryWrites=true', {
+mongoose.connect('mongodb+srv://juliandg34:j1a9n91996@cluster0-owv7k.mongodb.net/test?retryWrites=true', {
     useCreateIndex: true,
     useNewUrlParser: true
 }).then(db => console.log('Base de datos conectada'))
